f7ed8e083c45c9eb1c6e0061d9d9de577a3c1cdc
Feruzbek <quronboyevferuzbek0@gmail,com>
2025-04-05 15:30:52 +0500

    sss

--- obj/Debug/net8.0/C-_dev.AssemblyInfo.cs
+++ obj/Debug/net8.0/C-_dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-_dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f5a32049bfec4e54db7412ad89ac2e75fe4ebcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6abb3e42589303ea7c0faa4923a406dc8d9e11")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-_dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-_dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
